57f4dc65fe3bbef90dc93f6dd9418444dce85223
RasikaSonavane <rsika2014@gmail.com>
2021-09-04 21:27:31 +0530

    keywords and operators done

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 console.log("hello javascript")
 //variables are caseSensitive
+//var is keyword, keyword is the prdefined values
 var fullName="Rasika Sonavane"  
 var bankAccountNumber="123456789"
 var fullName="Rasika Son" //in java script duplication of variables get replace by new variable
@@ -7,7 +8,16 @@ console.log(fullName)
 console.log(bankAccountNumber)
 var mobileNumber
 console.log(mobileNumber) //mobileNumber is undefined data type coz we have not defined anything for this variable
+//always try to use proper variable names to make this more understandable by developers, to ease the project co-ordination
+ 
 
-
-
+//operator
+var x=2
+var y=4
+console.log(x+y) //addition
+console.log(x-y) //substraction
+console.log(x*y) //multiplication
+console.log(x/y) //dividation
+console.log(x%y) //modulus i.e gives you remainder
+console.log(x**y)
 
