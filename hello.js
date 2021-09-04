a2d89c715e0b1845c86bff382b4722e0fa5069b2
RasikaSonavane <rsika2014@gmail.com>
2021-09-04 21:02:10 +0530

    this is hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("hello javascript")
+//variables are caseSensitive
+var fullName="Rasika Sonavane"  
+var bankAccountNumber="123456789"
+var fullName="Rasika Son" //in java script duplication of variables get replace by new variable
+console.log(fullName)
+console.log(bankAccountNumber)
+var mobileNumber
+console.log(mobileNumber) //mobileNumber is undefined data type coz we have not defined anything for this variable
+
+
+
+
