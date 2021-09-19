89847128971c7cef77b79e668156c878ee308987
RasikaSonavane <rsika2014@gmail.com>
2021-09-19 21:12:58 +0530

    function object and methods

--- function.js
+++ function.js
@@ -19,9 +19,54 @@
   
   // Only change code above this line
   
-  var sum = 5
-function addSum(num) {
-  sume = sum + num
-  console.log(sume)
+ //var sum = 5
+//function addSum(num) {
+ // sume = sum + num
+ // console.log(sume)
+//}
+//addSum(5)
+/*var a=10
+function rand()
+{
+  a="rasika"
+console.log(a)
 }
-addSum(5)
+rand()
+var newrand=rand
+console.log("********")
+newrand()
+console.log(a)
+  function print(msg)
+  {
+    msg()
+  }
+  print(rand)
+
+  function printone(q,w){ q(w) 
+    q("12")}
+    printone(console.log, "13")
+ 
+    var rand=function(){a="rasika"
+  console.log(a)}
+
+  var rand=()=>{console.log("hi")}*/
+
+  function getAge()
+  {
+    var i=0
+    for(i=0;i<10;i=i+2)
+    {
+      if(i==8)
+      {return 20}
+    
+    console.log(i)
+    }
+  }
+  if(1<10)
+  {
+    return 30
+  }
+    console.log(60)
+
+
+console.log(getAge())
